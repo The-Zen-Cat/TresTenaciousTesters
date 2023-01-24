db3be4ad74ea6ab7443c461494f90ecba25e8b29
kptil <ktiley@ufl.edu>
2023-01-24 18:46:49 -0500

    comment - Mongoose

--- models/File.js
+++ models/File.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+/** This file creates a Mongoose schema and model. MongoDB holds JSON files or documents with potentially different 
+ * structures in collections. A schema imposes a structure on the JSON documents and the model is a constructor
+ * that takes in the schema and creates an instance of the document according to the schema. So the last line of 
+ * this file exports a model called "File", which is used in DAO.js line 42.
+ */
+
 const Schema = new mongoose.Schema({
 	Name: String,
 	ErrorCount: Number,
