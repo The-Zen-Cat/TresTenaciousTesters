1630041516fa0be710fded11fdefeb2a5e8c3afe
kptil <ktiley@ufl.edu>
2023-02-14 11:36:00 -0500

    Removed test files

--- testFiles/bellezzarachel_1207905_72036909_innerhtml.js
+++
@@ -1,6 +0,0 @@
-const scriptHTML = `<script>alert("Alert from innerHTML");</script>`;
-const main = document.getElementById('main');
-
-main.innerHTML = scriptHTML;
-
-document.getElementById("demo").innerHTML = "I have changed!";
