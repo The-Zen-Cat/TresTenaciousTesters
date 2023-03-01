5ddb46ebb9594f5660e7bc3f26a3a3e5651737ce
hpegah <73906163+hpegah@users.noreply.github.com>
2023-03-01 08:18:00 -0500

    added files to execute psalm at comming line

---
+++ psalm/main.py
@@ -0,0 +1,17 @@
+import subprocess
+import importlib.util
+import sys
+import json
+
+# defines the subprocess for running psalm
+
+def run_psalm():
+    if sys.argv[2] != "0": 
+        #list_files = subprocess.run(["py", "-m","bandit", "-r",sys.argv[1], "-f", "json"], stdout=subprocess.PIPE)
+        list_files = subprocess.run(["../vendor/bin/psalm",sys.argv[1]], stdout=subprocess.PIPE)
+        print(json.dumps(list_files.stdout.decode('utf-8'),ensure_ascii=False))
+    else:
+        list_files = subprocess.run(["bandit",sys.argv[1], "-f", "csv"])   
+
+if __name__ == '__main__':
+    run_psalm()
