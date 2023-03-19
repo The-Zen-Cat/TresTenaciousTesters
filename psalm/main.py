525e34d1cd212c2e25d54a17fc074a9ee0b9e173
hpegah <73906163+hpegah@users.noreply.github.com>
2023-03-19 11:21:34 -0700

    updated print message for psalm execution

--- psalm/main.py
+++ psalm/main.py
@@ -9,8 +9,7 @@ def run_psalm():
     if sys.argv[2] != "0": 
         # executes psalm on the file given in arg[1]
         list_files = subprocess.run(["./vendor/bin/psalm",sys.argv[1],"--taint-analysis"], stdout=subprocess.PIPE)
-        #print(json.dumps(list_files.stdout.decode('utf-8'),ensure_ascii=False))
-        print(list_files.stdout.decode('utf-8'))
+        print(json.dumps(list_files.stdout.decode('utf-8'),ensure_ascii=False))
     else:
         list_files = subprocess.run(["./vendor/bin/psalm",sys.argv[1],"--taint-analysis", "-f", "csv"])   
 
