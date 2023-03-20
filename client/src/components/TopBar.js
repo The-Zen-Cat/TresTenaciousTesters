aede1fda5cbad2c280cb76d14908dda6e2af13ee
Zachary Schirm <zaks@ufl.edu>
2023-03-20 11:57:46 -0400

    Fixes .csv blank download, spinner for upload, logout force refresh

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -4,44 +4,48 @@ import { useCookies } from "react-cookie";
 import { logout } from "../client/API.js";
 
 function TopBar(props) {
-
-	const { updateZipFileHandler, updateRouteHandler, userObject, callUpdateUserObject} = props;
-	const logout_wrapper = async (updateRouteHandler) =>
-	{
-		await logout();
-		callUpdateUserObject();
-		updateZipFileHandler("undefined");
-		updateRouteHandler("login");
-	};
-	return (
-		<div>
-			<Menu inverted attached="top">
-				<Menu.Menu position="right">
-					{ (userObject) &&
-						<Menu.Item
-							name="logout"
-							active
-							onClick={() => logout_wrapper(updateRouteHandler) }
-						></Menu.Item>
-					}
-					{ (!userObject) &&
-						<>
-							<Menu.Item
-								name="SignUp"
-								active
-								onClick={() => updateRouteHandler("SignUp") }
-							></Menu.Item>
-							<Menu.Item
-								name="LogIn"
-								active
-								onClick={() => updateRouteHandler("LogIn") }
-							></Menu.Item>
-						</>
-					}
-				</Menu.Menu>
-			</Menu>
-		</div>
-	);
+  const {
+    updateZipFileHandler,
+    updateRouteHandler,
+    userObject,
+    callUpdateUserObject,
+  } = props;
+  const logout_wrapper = async (updateRouteHandler) => {
+    await logout();
+    callUpdateUserObject();
+    updateZipFileHandler("undefined");
+    updateRouteHandler("login");
+    window.location.reload();
+  };
+  return (
+    <div>
+      <Menu inverted attached="top">
+        <Menu.Menu position="right">
+          {userObject && (
+            <Menu.Item
+              name="logout"
+              active
+              onClick={() => logout_wrapper(updateRouteHandler)}
+            ></Menu.Item>
+          )}
+          {!userObject && (
+            <>
+              <Menu.Item
+                name="SignUp"
+                active
+                onClick={() => updateRouteHandler("SignUp")}
+              ></Menu.Item>
+              <Menu.Item
+                name="LogIn"
+                active
+                onClick={() => updateRouteHandler("LogIn")}
+              ></Menu.Item>
+            </>
+          )}
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
 }
 
 export default TopBar;
